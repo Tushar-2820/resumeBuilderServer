79e45ee3bcc2616c4a78f97341d5d08f2f371141
Tushar-2820 <122382053+Tushar-2820@users.noreply.github.com>
2026-06-22 12:35:48 +0530

    Update server.js
    added process.env.port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { buildResume } = require("./resumeService");
 const { pdfDownload } = require("./pdfService");
 const cors = require('cors');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -28,6 +29,6 @@ app.post("/build-resume", async (req, res) => {
 
 app.post("/generatePdf", pdfDownload)
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server running on port 3000");
 });
