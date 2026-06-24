d2fb514973633e53761dfec106ecc88fcadfd9db
Tushar-2820 <122382053+Tushar-2820@users.noreply.github.com>
2026-06-24 10:33:03 +0530

    Update pdfService.js

--- pdfService.js
+++ pdfService.js
@@ -21,7 +21,7 @@ const pdfDownload = async (req, res) => {
                 </style>
                 <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css">
             </head>
-            <body>
+            <body class='bg-white p-10'>
                 ${html}
             </body>
             </html>
